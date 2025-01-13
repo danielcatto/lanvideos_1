209b179cfe9fe91a888c8c411d9db2836db29fcd
danielcatto <danielcatto@hotmail.com>
2025-01-13 20:20:32 -0300

    ajuste nos templates

--- app/my_video_club/urls.py
+++ app/my_video_club/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('videos.urls')),
+    path('', include('members.urls')),
     path('videos/', include('videos.urls')),
     path('admin/', admin.site.urls),
 ]
