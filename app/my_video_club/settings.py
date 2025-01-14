899577cb066a93cea78abb1dfd277db1fcd65279
danielcatto <danielcatto@hotmail.com>
2025-01-14 00:11:23 -0300

    configurar docker-compose serviço do postgre

--- app/my_video_club/settings.py
+++ app/my_video_club/settings.py
@@ -79,10 +79,16 @@ WSGI_APPLICATION = 'my_video_club.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'app/db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', 'change-me'),
+        'NAME': os.getenv('POSTGRES_DB', 'change-me'),
+        'USER': os.getenv('POSTGRES_USER', 'change-me'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'change-me'),
+        'HOST': os.getenv('POSTGRES_HOST', 'change-me'),
+        'PORT': os.getenv('POSTGRES_PORT', 'change-me'),
     }
 }
 
